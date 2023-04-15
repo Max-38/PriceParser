e316bd981e151270f13aa8564b68498448ba6178
Max <Ryadnov-97@yandex.ru>
2023-04-16 01:55:09 +0300

    Создать сущность товара

---
+++ Core/Domain/PriceParser/Position.cs
@@ -0,0 +1,16 @@
+﻿namespace PriceParser
+{
+    public class Position
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public Position (Guid id, string name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+    }
+}
